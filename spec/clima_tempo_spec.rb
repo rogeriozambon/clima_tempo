f6d8d6ce70604ee46eb9353ef0484a1d5edf58f9
Rogério Zambon <rogeriozambon@gmail.com>
2013-04-17 14:59:44 -0300

    Some changes in spec file

--- spec/clima_tempo_spec.rb
+++ spec/clima_tempo_spec.rb
@@ -5,7 +5,7 @@ describe ClimaTempo do
     expect { climatempo = ClimaTempo.new }.to raise_error(ArgumentError)
   end
 
-  context "Values up to this moment" do
+  context "values for the time" do
     before do
       fixture = File.open("spec/fixture/sao_paulo.html").read
       FakeWeb.register_uri :any, "http://www.climatempo.com.br/previsao-do-tempo/cidade/558/empty", :body => fixture
@@ -13,20 +13,34 @@ describe ClimaTempo do
       @climatempo = ClimaTempo.new :code => 558
     end
 
-    it "check object types" do
+    it "object type" do
       @climatempo.now.should be_an(Hash)
     end
 
-    it "check hash keys" do
+    it "keys" do
       hash = @climatempo.now
+
+      hash.should have(7).items
 
       hash.should have_key(:temperature)
       hash.should have_key(:wind)
       hash.should have_key(:condition)
       hash.should have_key(:pressure)
       hash.should have_key(:intensity)
-      hash.should have_key(:humidity)
+      hash.should have_key(:moisture)
       hash.should have_key(:video)
+    end
+
+    it "values" do
+      hash = @climatempo.now
+
+      hash[:temperature].should == "24ºC"
+      hash[:wind].should == "Sudeste"
+      hash[:condition].should == "Muitas nuvens"
+      hash[:pressure].should == "1021 hPa"
+      hash[:intensity].should == "19 Km/h"
+      hash[:moisture].should == "57%"
+      hash[:video].should include("grande-sp")
     end
   end
 end
