3886e5a4bf132d782fe93133a36ddd33691d1a95
Rogério Zambon <rogeriozambon@gmail.com>
2013-05-03 14:33:44 -0300

    Some changes. Removed "video" key

--- spec/clima_tempo_spec.rb
+++ spec/clima_tempo_spec.rb
@@ -1,26 +1,31 @@
 require "spec_helper"
 
 describe ClimaTempo do
-  it "missing parameters" do
-    expect { climatempo = ClimaTempo.new }.to raise_error(ArgumentError)
+  it "missing type" do
+    expect { climatempo = ClimaTempo.new(:code) }.to raise_error(TypeError)
   end
 
-  context "values for the time" do
+  it "missing parameters" do
+    expect { climatempo = ClimaTempo.new }.to raise_error(ArgumentError)
+    expect { climatempo = ClimaTempo.new({}) }.to raise_error(ArgumentError)
+  end
+
+  context "values from São Paulo" do
     before do
       fixture = File.open("spec/fixture/sao_paulo.html").read
       FakeWeb.register_uri :any, "http://www.climatempo.com.br/previsao-do-tempo/cidade/558/empty", :body => fixture
 
-      @climatempo = ClimaTempo.new :code => 558
+      @clima_tempo = ClimaTempo.new :code => 558
     end
 
     it "object type" do
-      @climatempo.now.should be_an(Hash)
+      @clima_tempo.now.should be_an(Hash)
     end
 
-    it "keys" do
-      hash = @climatempo.now
+    it "object keys" do
+      hash = @clima_tempo.now
 
-      hash.should have(7).items
+      hash.should have(6).items
 
       hash.should have_key(:temperature)
       hash.should have_key(:wind)
@@ -28,19 +33,17 @@ describe ClimaTempo do
       hash.should have_key(:pressure)
       hash.should have_key(:intensity)
       hash.should have_key(:moisture)
-      hash.should have_key(:video)
     end
 
-    it "values" do
-      hash = @climatempo.now
+    it "object values" do
+      hash = @clima_tempo.now
 
-      hash[:temperature].should == "24ºC"
-      hash[:wind].should == "Sudeste"
-      hash[:condition].should == "Muitas nuvens"
-      hash[:pressure].should == "1021 hPa"
-      hash[:intensity].should == "19 Km/h"
-      hash[:moisture].should == "57%"
-      hash[:video].should include("grande-sp")
+      hash[:temperature].should == "28ºC"
+      hash[:wind].should == "Norte"
+      hash[:condition].should == "Tempo firme"
+      hash[:pressure].should == "1017 hPa"
+      hash[:intensity].should == "11 Km/h"
+      hash[:moisture].should == "51%"
     end
   end
 end
